7ceedd020648e66390c80472a51dad5275a7e9a8
Omid Raha <or@omidraha.com>
2023-10-17 16:55:14 -0700

    Update example

--- longhorn/__main__.py
+++ longhorn/__main__.py
@@ -3,14 +3,22 @@ from base.cluster import create_cluster
 from base.const import NAMESPACE_NAME
 from base.namespace import create_namespace
 from base.provider import create_provider
+from base.security_group import create_security_group
+from base.utils import get_public_keys
 from base.vpc import create_eip, create_vpc
 import pulumi
 
 
 def setup():
+    public_keys = get_public_keys()
     eip = create_eip()
     vpc = create_vpc(eip)
-    cluster = create_cluster(vpc)
+    security_group = create_security_group(vpc)
+    cluster = create_cluster(
+        vpc,
+        security_group,
+        public_keys[0],
+    )
     provider = create_provider(cluster)
     namespace = create_namespace(provider, NAMESPACE_NAME)
     pulumi.export("kubeconfig", cluster.kubeconfig)
